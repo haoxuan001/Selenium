bbf80f95837efec3631819253527f15ddd3bbd11
haoxuan001 <34461452+haoxuan001@users.noreply.github.com>
2024-07-09 22:11:01 +1000

    initial commit

---
+++ src/main/java/com/planit/jupiter/App.java
@@ -0,0 +1,13 @@
+package com.planit.jupiter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
